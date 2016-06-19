e4b2141de25b0305b74529883c205d00f6e09e60
tinSn0703 <condensa1952@gmail.com>
2016-06-19 16:39:23 +0900

    1
    とりあえず現行のH28_AVRに合わせた

--- H28_CI/main.cpp
+++ H28_CI/main.cpp
@@ -6,17 +6,17 @@
  */ 
 
 #include <avr/io.h>
-#include <H28_AVR.h>
+#include <H28_AVR/H28_AVR.h>
 #include <FallMoon/akilcd_ci.h>
 
-#define led_sw_0	EN_IO0
-#define led_sw_1	EN_IO1
-#define led_sw_2	EN_IO2
-#define led_sw_3	EN_IO3
-#define led_link_0	EN_IO4
-#define led_out_0	EN_IO5
-#define led_link_1	EN_IO6
-#define led_out_1	EN_IO7
+#define led_sw_0	EI_IO0
+#define led_sw_1	EI_IO1
+#define led_sw_2	EI_IO2
+#define led_sw_3	EI_IO3
+#define led_link_0	EI_IO4
+#define led_out_0	EI_IO5
+#define led_link_1	EI_IO6
+#define led_out_1	EI_IO7
 
 void F_In_BT_slave(C_UART_R _arg_uart_r,T_DATA _arg_in_data[])
 {	
@@ -24,7 +24,7 @@ void F_In_BT_slave(C_UART_R _arg_uart_r,T_DATA _arg_in_data[])
 	
 	_arg_uart_r.Check();
 	
-	if (_arg_uart_r.Ret_flag() == EF_ERROR)
+	if (_arg_uart_r.Ret_flag() == EU_ERROR)
 	{
 		_arg_in_data[0] = 0x3f;
 		_arg_in_data[1] = 0x7f;
@@ -48,20 +48,20 @@ int main(void)
 {
 	C_UART_R uart_r_bt[2] = 
 	{
-		C_UART_R(EN_UART0),
-		C_UART_R(EN_UART1),
+		C_UART_R(EU_UART0),
+		C_UART_R(EU_UART1),
 	};
 	
-	C_UART_T uart_t_main(EN_UART0);
+	C_UART_T uart_t_main(EU_UART0);
 	
-	C_IO_OUT io_o_led(EN_PORTA,0xff);
+	C_IO_OUT io_o_led(EI_PORTA,0xff);
 	
-	C_IO_IN io_i_sw(EN_PORTB,0x1e);
+	C_IO_IN io_i_sw(EI_PORTB,0x1e);
 	
 	C_IO_IN_pin io_i_link[2] =
 	{
-		C_IO_IN_pin(EN_PORTD,EN_IO4),
-		C_IO_IN_pin(EN_PORTD,EN_IO5)
+		C_IO_IN_pin(EI_PORTD,EI_IO4),
+		C_IO_IN_pin(EI_PORTD,EI_IO5)
 	};
 	
 	INI_LCD();
